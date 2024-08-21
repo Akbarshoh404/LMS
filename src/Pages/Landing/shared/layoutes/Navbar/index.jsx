99005caaf6f60d5e532b9cc42ccb47b03ba7eee6
Akbarshoh <ismatov2009akb@gmail.com>
2024-08-21 22:10:06 +0500

    Navbar update

--- src/Pages/Landing/shared/layoutes/Navbar/index.jsx
+++ src/Pages/Landing/shared/layoutes/Navbar/index.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+// src/Navbar.js
+import React, { useState } from "react";
+import styles from "./style.module.css";
+
+import logo from "../../assets/images/logo.png";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Navbar
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.container}>
+        <div className={styles.logoDiv}>
+          <img src={logo} alt="Logo" />
+
+          <p>
+            Learning
+            <br />
+            Management System
+          </p>
+        </div>
+
+        <div className={`${styles.menu} ${isOpen ? styles.active : ""}`}>
+          <a href="#home">About us</a>
+          <a href="#about">Schools</a>
+          <a href="#services">Teachers</a>
+          <a className={styles.loginButton}>Login</a>
+          <a className={styles.registerButton}>Sign Up</a>
+        </div>
+
+        <div className={styles.hamburger} onClick={toggleMenu}>
+          &#9776;
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
