e1701319c96115d9c34b5c642a77913a85951c23
Akbarshoh <ismatov2009akb@gmail.com>
2024-08-23 01:40:47 +0500

    Contact Us section

--- src/Pages/Landing/Home/Contact Us/index.jsx
+++ src/Pages/Landing/Home/Contact Us/index.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React from "react";
 
 import styles from "./style.module.css";
 
+import img from "../../shared/assets/images/form-img.png";
+
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
-  )
-}
+    <>
+      <div className={styles.contact}>
+        <div className={styles.container}>
+          <div className={styles.title}>
+            <div className={styles.titleName}>Contact Us</div>
+            <div className={styles.titleText}>
+              Connect with Us: Let's Discuss Your School’s Learning Management
+              System
+            </div>
+          </div>
+          <div className={styles.form}>
+            <div className={styles.form2}>
+              <div>
+                <p>Name</p>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className={styles.input1}
+                />
+              </div>
 
-export default ContactUs
+              <div>
+                <p>Email</p>
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className={styles.input1}
+                />
+              </div>
+
+              <div>
+                <p>Message</p>
+                <input
+                  type="text"
+                  placeholder="Message"
+                  className={styles.input2}
+                />
+              </div>
+
+              <button>Send Message</button>
+            </div>
+
+            <img src={img} alt="" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ContactUs;
