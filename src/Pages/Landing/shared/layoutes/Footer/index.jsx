25f456a9cd46ff94d80513e123c418c6f6687a5e
Akbarshoh <ismatov2009akb@gmail.com>
2024-08-22 23:40:03 +0500

    footer update

--- src/Pages/Landing/shared/layoutes/Footer/index.jsx
+++ src/Pages/Landing/shared/layoutes/Footer/index.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from "react";
 
-import styles from "./style.module.css"
+import styles from "./style.module.css";
+
+import logo from "../../assets/images/logo_white.png";
+
+import LinkedIn from "../../assets/images/social_linkedIn.png";
+import Facebook from "../../assets/images/social_Facebook.png";
+import Twitter from "../../assets/images/social_Twitter.png";
 
 const LandingFooter = () => {
   return (
-    <div>LandingFooter</div>
-  )
-}
+    <>
+      <div className={styles.footer}>
+        <div className={styles.container}>
+          <div className={styles.top}>
+            <div className={styles.logoDiv}>
+              <img src={logo} alt="Logo" />
 
-export default LandingFooter
+              <p>
+                Learning
+                <br />
+                Management System
+              </p>
+            </div>
+
+            <div className={styles.navigation}>
+              <p>About us</p>
+
+              <p>Schools</p>
+
+              <p>Teachers</p>
+            </div>
+
+            <div className={styles.social}>
+              <img src={LinkedIn} alt="LinkedIn" />
+              <img src={Facebook} alt="Facebook" />
+              <img src={Twitter} alt="Twitter" />
+            </div>
+          </div>
+          <div className={styles.middle}></div>
+          <div className={styles.bottom}>
+            <p>© 2024 LMS. All Rights Reserved.</p>
+            <p>Privacy Policy</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LandingFooter;
