9cc743aa3aab9dbc84342f0ee6ce4b40af0cf1ca
AsilbekMelikov <melikovasilbek04@gmail.com>
2024-08-28 18:20:58 +0500

    Finalize the services section | transfer assets folder | create jsconfig file

--- src/Pages/Landing/Home/index.jsx
+++ src/Pages/Landing/Home/index.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import Navbar from "../shared/layoutes/Navbar/index";
 import Header from "./Header/index";
 import Services from "./Services/index";
@@ -8,8 +6,6 @@ import CaseStudies from "./Case Studies/index";
 import ContactUs from "./Contact Us/index";
 import Teachers from "./Teachers";
 import Footer from "../shared/layoutes/Footer/index";
-
-import styles from "./style.module.css";
 
 const LandingHome = () => {
   return (
